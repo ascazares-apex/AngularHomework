d4f757f063cefd75f6f869405ec9cbb16079d35b
Asoka Cazares <ascazares@apexsystems.com>
2024-03-28 01:15:55 -0600

    Solution for Exercise #2
    Use of Structural Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AttributeDirectivesComponent } from './components/attribute-directives/attribute-directives.component';
+import { StructuralDirectivesComponent } from './components/structural-directives/structural-directives.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AttributeDirectivesComponent
+    AttributeDirectivesComponent,
+    StructuralDirectivesComponent
   ],
   imports: [
     BrowserModule,
